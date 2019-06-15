8c6d56454e36123969f4016c2f114bb95e2cb42b
adwilks <adrien.wilkins@ukiyo.tech>
2019-06-15 17:45:05 +0900

    Initial commit: Create tableview, create DoItviewcontroller. Table responds to cell selections, sets checkmark on cell.

---
+++ Do It/DoItViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Do It
+//
+//  Created by Adrien Wilkins on 6/15/19.
+//  Copyright © 2019 Ukiyo LLC. All rights reserved.
+//
+
+import UIKit
+
+class DoItViewController: UITableViewController {
+
+    // Mark - TableView Datasource methods
+    let tableArray = ["Learn Karate", "Master art", "Defeat Sensei", "Become the master"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = tableArray[indexPath.row]
+        return cell
+    }
+    
+    //Mark - TableView Delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO Write method for what to do on row selection
+       
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
